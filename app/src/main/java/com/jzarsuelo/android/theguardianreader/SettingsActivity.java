a58021b8848d58eedcd20dbf69242d2a5174e297
Pao <jzarsuelo.dev@gmail.com>
2017-02-06 09:41:18 +0800

    [US02] - Added preference

---
+++ app/src/main/java/com/jzarsuelo/android/theguardianreader/SettingsActivity.java
@@ -0,0 +1,23 @@
+package com.jzarsuelo.android.theguardianreader;
+
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    public static class NewsPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.settings_main);
+        }
+    }
+}
