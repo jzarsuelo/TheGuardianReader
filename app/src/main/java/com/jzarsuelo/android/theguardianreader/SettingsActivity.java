1a0b1eddc7c057391db520296e967b0fb5dee4eb
Pao <jzarsuelo.dev@gmail.com>
2017-02-06 10:28:30 +0800

    [US02] - Added Up Button in App's Preference - Added Preference Summary

--- app/src/main/java/com/jzarsuelo/android/theguardianreader/SettingsActivity.java
+++ app/src/main/java/com/jzarsuelo/android/theguardianreader/SettingsActivity.java
@@ -1,6 +1,9 @@
 package com.jzarsuelo.android.theguardianreader;
 
+import android.content.SharedPreferences;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,12 +15,29 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
     }
 
-    public static class NewsPreferenceFragment extends PreferenceFragment {
+    public static class NewsPreferenceFragment extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference newsToLoad = findPreference( getString(R.string.settings_news_to_load_key) );
+            bindPreferenceToSummaryValue(newsToLoad);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+        private void bindPreferenceToSummaryValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 }
