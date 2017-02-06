8da5f1cf439f367678ddf067783e06b34b930cf3
Pao <jzarsuelo.dev@gmail.com>
2017-02-06 09:37:29 +0800

    [US02] - Added menu

--- app/src/main/java/com/jzarsuelo/android/theguardianreader/MainActivity.java
+++ app/src/main/java/com/jzarsuelo/android/theguardianreader/MainActivity.java
@@ -5,8 +5,13 @@ import android.app.LoaderManager;
 import android.content.Intent;
 import android.content.Loader;
 import android.net.Uri;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -64,6 +69,18 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+
+        int tintColor = ContextCompat.getColor(this, android.R.color.white);
+        MenuItem settingsMenu = menu.findItem(R.id.settings);
+        DrawableCompat.setTint(settingsMenu.getIcon(), tintColor);
+
+        return true;
     }
 
     @Override
